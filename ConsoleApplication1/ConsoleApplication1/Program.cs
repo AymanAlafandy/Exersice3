c32935c717a1499df64eac63fe67f63cc8cb2a64
nicolepintea <nicole.pintea@gmail.com>
2017-10-24 17:07:06 +0200

    done 80%

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,51 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
+            AddPerson();
+        }
+            private static void AddPerson()
+            {
+            Person person = new Person();
+            Console.Write("FName: ");
+            var FName = Console.ReadLine();
+            if (string.IsNullOrEmpty(FName))
+            {
+               // return false;
+            }
+            Console.Write("LName: ");
+            var LName = Console.ReadLine();
+            if (string.IsNullOrEmpty(LName))
+            {
+              //  return false;
+            }
+            Console.Write("Age: ");
+            string Age=Console.ReadLine();
+            int age0 = 0;
+            if (!int.TryParse(Age, out age0))
+            {
+                Console.WriteLine("Age? Age could not be parsed");
+            }
+            Console.Write("Height: ");
+            string Height =Console.ReadLine();
+           float height0 = 0;
+            if (!float.TryParse(Height, out height0))
+            {
+                Console.WriteLine("Height? Height could not be parsed");
+            }
+            Console.Write("Weight: ");
+            string Weight = Console.ReadLine();
+            float weight0 = 0;
+            if (!float.TryParse(Weight, out weight0))
+            {
+                Console.WriteLine("Weight? Weight could not be parsed");
+            }
+            PersonHandler pershand = new PersonHandler();
+            pershand.CreatePerson(age0, FName, LName, height0, weight0);
+          
+            Console.WriteLine("HI" + " "+ FName + " " + LName + ", Age" + age0 + ", Height" + height0 + ", Weight" + weight0);
+            Console.ReadLine();
+             
         }
     }
 }
