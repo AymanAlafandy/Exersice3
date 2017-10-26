248fe2a013ebd10c26646b5ff358878e8faa65de
nicolepintea <nicole.pintea@gmail.com>
2017-10-26 09:16:33 +0200

    Partialy done

---
+++ ConsoleApplication1/ConsoleApplication1/PersonHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class PersonHandler
+    {
+       
+        public void SetAge(Person pers, int age)
+            
+           
+        {
+            pers.Age=age;
+        }
+
+        public Person CreatePerson(int age, string fname, string lname, float height, float weight)
+        {
+            Person person = new Person();
+            person.LName = lname;
+            person.FName = fname;
+            person.Age = age;
+            person.Height = height;
+            person.Weight = weight;
+            return person;
+        }
+
+        //private List<Person> list = new List<Person>();
+        //public void AddPerson(Person person)
+        //{
+        //    list.AddPerson(person);
+        //}
+        //public void(List)()
+        //{
+        //    foreach (var person in list)
+	       // {
+        //    Console.WriteLine(person);
+	       // }
+        //}
+        //public override string ToString()
+        //{
+        //    var result = "";
+        //    foreach (var person in list)
+	       // {
+        //     result += person + "n";
+        //    }
+        //return result;
+
+	    }
+    }
+
